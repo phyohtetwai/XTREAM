ac4e19fb657f193113edf123b563e394065585c4
K0PHY03 <43138249+K0PHY03@users.noreply.github.com>
2021-01-27 19:41:18 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-apt-get update ; apt-get install libxslt1-dev libcurl3 libgeoip-dev python -y ; wget https:///xui/install.py ; sudo python install.py
+apt-get update ; apt-get install libxslt1-dev libcurl3 libgeoip-dev python -y ; wget https://raw.githubusercontent.com/K0PHY03/Xtream/main/install.py ; sudo python install.py

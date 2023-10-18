22434c1e269be2dfa665136048b42bcacb54f8fe
Phyo Htet Wai <43138249+phyohtetwai@users.noreply.github.com>
2023-10-18 10:25:20 +0630

    Update require.sh

--- require.sh
+++ require.sh
@@ -1,1 +1,1 @@
-apt-get update ; apt-get install libxslt1-dev libcurl3 libgeoip-dev python -y ; sudo python xtream_codes.py
+apt-get update ; apt-get upgrade; apt-get install libxslt1-dev libcurl3 libgeoip-dev python -y ; sudo python xtream_codes.py
